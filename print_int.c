5325886554c844b137aa5fb0bf9e9fc939e9db5b
JD037 <joj237@gmail.com>
2023-03-15 17:28:48 -0700

    changed print_int.c back to previous code

--- print_int.c
+++ print_int.c
@@ -10,32 +10,32 @@ int print_int(va_list arg)
 {
 	int num = va_arg(arg, int);
 	int count = 0;
-	int num_copy = num < 0 ? -num : num;
+	int num_copy = num;
 	int num_digits = 0;
 	int divisor = 1;
 	int i;
 
-	if (num == 0)
-		return (_putchar('0'));
-
 	if (num < 0)
 	{
 		count += _putchar('-');
-		num_copy = -num;
+		num = -num;
 	}
 
-	while (num_copy / divisor > 0)
+	while (num_copy != 0)
 	{
-		divisor *= 10;
+		num_copy /= 10;
 		num_digits++;
 	}
 
-	divisor /= 10;
+	for (i = 1; i < num_digits; i++)
+		divisor *= 10;
 
-	for (i = 0; i < num_digits; i++)
+	while (divisor != 0)
 	{
-		count += _putchar('0' + num_copy / divisor);
-		num_copy %= divisor;
+		int digit = num / divisor;
+
+		count += _putchar(digit + '0');
+		num %= divisor;
 		divisor /= 10;
 	}
 
